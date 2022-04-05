3697efd7d113b4b5938c8a7633440caa455cc181
888jonathan <888jonathan@gmail.com>
2022-04-05 22:31:32 +0800

    add calculation of pd, i

--- LendingFair0405.R
+++ LendingFair0405.R
@@ -8,15 +8,45 @@ setwd(dirname(current_path))
 
 #list.files("./data")
 df <- read_csv("./data/Loan_status_2007-2020Q3.gzip", col_names = TRUE,
-               col_select = c("id", "issue_d", "grade", "loan_status"))
+               col_select = c("issue_d", "grade", "loan_status", "installment", "int_rate"))
 
 df$issue_d <- my(df$issue_d)
+df$int_rate <- parse_number(df$int_rate)
 
-asdf <- filter(df, issue_d < "2018-01-01", issue_d > "2016-12-01",
-               loan_status %in% c("Charged Off", "Default", "Fully Paid")) %>% 
+asdf <- filter(df, issue_d < "2018-01-01", issue_d > "2015-12-01",
+               loan_status %in% c("Charged Off", "Fully Paid")) %>% 
   group_by(grade) %>% 
   summarise(n = n()) %>% 
   mutate(freq = n/ sum(n))
 
-#haha
-# sthsth
+num <-asdf <- filter(df, issue_d < "2017-10-01", issue_d > "2015-12-01") %>% 
+  group_by(issue_d) %>% 
+  summarise(n = n()) %>% 
+  mutate(freq = n/ sum(n))
+
+# Average interest rate by grades
+
+a1 <- filter(df, issue_d < "2018-01-01", issue_d > "2009-12-01") %>%
+  group_by(grade) %>% 
+  summarise(n = n(), avg_int=mean(int_rate)) %>% 
+  mutate(grade_freq = n/ sum(n)) %>% 
+  ungroup()
+
+a2 <- filter(df, issue_d < "2018-01-01", issue_d > "2009-12-01", loan_status %in% ("Charged Off")) %>%
+  group_by(grade) %>% 
+  count(loan_status) %>% 
+  ungroup() %>% 
+  mutate(prob_default =n / a1["n"])
+
+b1 <- filter(df, issue_d < "2017-10-01", issue_d > "2015-12-01") %>%
+  group_by(grade) %>% 
+  summarise(n = n(), avg_int=mean(int_rate)) %>% 
+  mutate(grade_freq = n/ sum(n)) %>% 
+  ungroup()
+
+b2 <- filter(df, issue_d < "2017-10-01", issue_d > "2015-12-01", loan_status %in% ("Charged Off")) %>%
+  group_by(grade) %>% 
+  count(loan_status) %>% 
+  ungroup() %>% 
+  mutate(prob_default =n / a1["n"])
+
